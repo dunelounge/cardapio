0fbf6000543d8b84ac084fba30669f1c67c63870
nrodr <nrodriguesalves05@gmail.com>
2026-05-06 00:18:14 -0300

    atualização bossa nova

--- js/script.js
+++ js/script.js
@@ -5,18 +5,21 @@ const menuData = {
             { name: 'Combo Beefeater', description: '4 gelo + red bull sabores', price: '270,00', image: 'img/beefeter.jpg' },
             { name: 'Combo Beefeater Pink', description: '4 gelo + red bull sabores', price: '300,00', image: 'img/beefeter sabor.jpg' },
             { name: 'Combo Tanqueray', description: '4 gelo + red bull sabores', price: '300,00', image: 'img/tanquery.jpg' },
+            { name: 'Combo Tanqueray Royale', description: '4 gelo + red bull sabores', price: '340,00' },
             { name: 'Combo Absolut', description: '4 gelo + red bull sabores', price: '230,00', image: 'img/absolut.jpg' },
             { name: 'Combo Ciroc Blue', description: '4 gelo + red bull sabores', price: '340,00', image: 'img/ciroc.jpg' },
             { name: 'Combo Ciroc Red Berry', description: '4 gelo + red bull sabores', price: '360,00', image: 'img/ciroc_sabor.jpg' },
             { name: 'Combo Grey Goose', description: '4 gelo + red bull sabores', price: '300,00', image: 'img/grey_goose_azul.jpg' },
             { name: 'Combo Black Label', description: '4 gelo + red bull', price: '340,00', image: 'img/black.jpg' },
+            { name: 'Combo Double Black', description: '4 gelo + red bull', price: '380,00' },
             { name: 'Combo Buchannas', description: '4 gelo + red bull', price: '360,00', image: 'img/buchanas.jpg' },
             { name: 'Combo Chivas', description: '4 gelo + red bull', price: '300,00', image: 'img/chivas.jpg' },
             { name: 'Combo Gold Label', description: '4 gelo + red bull', price: '500,00', image: 'img/goldlabel.jpg' },
             { name: 'Combo Jack Daniels', description: '4 gelo + red bull', price: '300,00', image: 'img/jack.jpg' },
+            { name: 'Combo Jack Honey', description: '4 gelo + red bull', price: '330,00' },
+            { name: 'Combo Jack Maçã Verde', description: '4 gelo + red bull', price: '330,00' },
             { name: 'Combo Old Parr', description: '4 gelo + red bull', price: '300,00', image: 'img/oldpar.jpg' },
             { name: 'Combo Red Label', description: '4 gelo + red bull', price: '250,00', image: 'img/red label.jpg' },
-            { name: 'Combo Tanqueray Bossa Nova', description: '4 gelo + red bull sabores', price: '340,00' }, // NOVO ITEM
         ]
     },
     porcoes: {
@@ -26,10 +29,7 @@ const menuData = {
             { name: 'Batata Frita C Calabresa', description: '500g', price: '40,00' },
             { name: 'Batata Frita C/ Cheddar e Bacon', description: '500g', price: '45,00', image: 'img/batata.jpg', featured: 'Sugestão da Casa' },
             { name: 'Calabresa', description: '500g', price: '40,00', image: 'img/calabresa.jpg' },
-            // { name: 'Frango a Passarinho', description: '500g', price: '35,00', image: 'img/frango.jpg' },
             { name: 'Cebola Empanada', description: '500g', price: '30,00', image: 'img/cebolav2.jpg' },
-            // { name: 'Contra Filé na Chapa', description: '500g', price: '70,00', image: 'img/contrav2.jpg' },
-            // { name: 'Picanha na Chapa', description: '500g', price: '90,00', image: 'img/picanha.jpg' },
         ]
     },
     drinks: {
@@ -38,8 +38,6 @@ const menuData = {
             { name: 'Caipirinha', description: 'Cachaça com Morango, Kiwi, Maracujá, Limão ou Frutas Vermelhas', price: '25,00', image: 'img/caipirinha_morango.jpg' },
             { name: 'Caipiroska', description: 'Vodka com Morango, Kiwi, Maracujá, Limão ou Frutas Vermelhas', price: '30,00', image: 'img/caipirinha_mara.jpg' },
             { name: 'Sakeirinha', description: 'Vodka com Morango, Kiwi, Maracujá, Limão ou Frutas Vermelhas', price: '30,00', image: 'img/caipirinha_limao.jpg' },
-            // { name: 'Dune Kiss', description: 'Gin, licor 43, limão siciliano e mel infusionado', price: '35,00', featured: 'Exclusivo', image: 'img/dunekiss.JPG' },
-            // { name: 'Sabi Night', description: 'Gin, xarope de rosas francesas, morango e limão siciliano.', price: '30,00', image: 'img/sabi.jpg', featured: 'Exclusivo'},
             { name: 'Whiskey Sour', description: 'Bourbon, limão-siciliano, xarope de açúcar e Angostura.', price: '40,00', image: 'img/whiskey_sour.jpg' },
             { name: 'Clericot', description: 'Espumante, Licor 43, morango, kiwi e água com gás.', price: '40,00', image: 'img/cleri.jpg' },
             { name: 'Aperol Spritz', description: 'Aperol, espumante, água com gás e laranja.', price: '40,00' },
@@ -55,8 +53,7 @@ const menuData = {
         title: 'Clube do Gin',
         items: [
             { name: 'Gin Ballena', description: '', price: '45,00', image: 'img/balena.jpg', featured: 'Exclusivo' },
-            { name: 'Gin Clássico', description: '', price: '40,00', description: 'Gin, Tônica' },
-            // { name: 'Gold Dune', description: 'Gin, licor 43, limão siciliano e tônica', price: '55,00', featured: 'Exclusivo' },
+            { name: 'Gin Clássico', description: 'Gin, Tônica', price: '40,00' },
             { name: 'Gin Mulle', description: 'Gin, limão siciliano, schweppes citrus e espuma de gengibre.', price: '45,00' },
             { name: 'Gin Rose', description: 'Gin, xarope de rosas francesas e tônica', price: '45,00', image: 'img/gin.jpg', featured: 'Sugestão da Casa' },
             { name: 'Gin Tropical', description: 'Gin, mix de frutas e red bull tropical', price: '45,00', image: 'img/gintropical.webp' },
@@ -70,6 +67,7 @@ const menuData = {
             { name: 'Dose Beefeater', description: '1 Red bull + 1 gelo sabores', price: '40,00' },
             { name: 'Dose Beefeater Pink', description: '1 Red bull + 1 gelo sabores', price: '45,00' },
             { name: 'Dose Black Label', description: '1 Red bull + 1 gelo sabores', price: '45,00' },
+            { name: 'Dose Double Black', description: '1 Red bull + 1 gelo sabores', price: '60,00' },
             { name: 'Dose Buchannas', description: '1 Red bull + 1 gelo sabores', price: '45,00' },
             { name: 'Dose Chivas', description: '1 Red bull + 1 gelo sabores', price: '40,00' },
             { name: 'Dose Ciroc Blue', description: '1 Red bull + 1 gelo sabores', price: '45,00' },
@@ -77,10 +75,12 @@ const menuData = {
             { name: 'Dose Gold Label', description: '1 Red bull + 1 gelo sabores', price: '55,00' },
             { name: 'Dose Grey Goose', description: '1 Red bull + 1 gelo sabores', price: '45,00' },
             { name: 'Dose Jack Daniels', description: '1 Red bull + 1 gelo sabores', price: '40,00' },
+            { name: 'Dose Jack Honey', description: '1 Red bull + 1 gelo sabores', price: '45,00' },
+            { name: 'Dose Jack Maçã Verde', description: '1 Red bull + 1 gelo sabores', price: '45,00' },
             { name: 'Dose Old Parr', description: '1 Red bull + 1 gelo sabores', price: '40,00' },
             { name: 'Dose Red Label', description: '1 Red bull + 1 gelo sabores', price: '35,00' },
             { name: 'Dose Tanqueray', description: '1 Red bull + 1 gelo sabores', price: '40,00' },
-            { name: 'Dose Tanqueray Bossa Nova', description: '', price: '50,00' }, // NOVO ITEM
+            { name: 'Dose Tanqueray Royale', description: '1 Red bull + 1 gelo sabores', price: '50,00' },
             { name: 'Gelo Tropical', description: '', price: '6,00' },
             { name: 'Gelo Coco', description: '', price: '4,00' },
             { name: 'Gelo Maracujá', description: '', price: '4,00' },
@@ -94,32 +94,18 @@ const menuData = {
     cervejas: {
         title: 'Cervejas',
         items: [
-            // Balde 5 Heineken long 55,00
-            // Balde 5 Budweiser long 45,00
-            // Balde 5 Corona long 65,00
-
-            // Balde 5 Heineken 600 80,00
-            // Balde 5 amstel 600 65,00
-            // Balde 5 original 600 65,00
-            // BALDES ADICIONADOS COM FOTO
             { name: 'Balde Heineken Long neck (5 Unidades)', price: '55,00', image: 'img/heineken_2.jpg' },
             { name: 'Balde Budweiser Long neck (5 Unidades)', price: '45,00', image: 'img/bud.jpg' },
             { name: 'Balde Corona Long neck (5 Unidades)', price: '65,00', image: 'img/corona.jpg' },
-
-            { name: 'Balde Heineken 600ml (5 Unidades)', price: '80,00' },
-            { name: 'Balde Brahma 600ml (5 Unidades)', price: '65,00' },
-            { name: 'Balde Amstel 600ml (5 Unidades)', price: '65,00', image: 'img/amstel.jpg' },
-            { name: 'Balde Original 600ml (5 Unidades)', price: '65,00', image: 'img/original.jpg' },
-
-            { name: 'Amstel 600 ML', price: '14,00' },
-            { name: 'Brahma 600 ML', price: '14,00' },
-            { name: 'Original 600 ML', price: '14,00' },
-            { name: 'Heineken 600 ML', price: '17,00' },
+        ]
+    },
+    longnecks: {
+        title: 'Long Necks',
+        items: [
             { name: 'Budweiser Long Neck', price: '10,00' },
             { name: 'Corona Long Neck', price: '14,00' },
             { name: 'Heineken Long Neck Zero', price: '12,00' },
-            { name: 'Skol Beats', price: '12,00' },
-            { name: 'Cabaré Ice', price: '12,00' },
+            { name: 'Skol Beats Sense', price: '12,00' },
             { name: 'Ice', price: '12,00' },
         ]
     },
@@ -128,7 +114,10 @@ const menuData = {
         collapsible: true,
         items: [
             { name: 'Aluguel', price: '30,00' },
-            { name: 'Essências Ziggy / Onix', description: 'Banana e açaí, Cherry, Coru magic, Frutas verdes, Tutti frutti, Grapp, Hapocalyx mint, High fusion, High herry, Hortelã, Laranja e morango, Limão, Manga tropical, Maçã, Pera, Tropical, Watermelon bomb, Frutas amarelas, Laranja, High passion, Duas goibas, Jabuticaba, Love 66', price: '25,00' },
+            { name: 'Aluguel Love 66', price: '40,00' },
+            { name: 'Troca Love 66', price: '35,00' },
+            { name: 'Essências Ziggy / Onix', description: 'Banana e açaí, Cherry, Coru magic, Frutas verdes, Tutti frutti, Grapp, Hapocalyx mint, High fusion, High herry, Hortelã, Laranja e morango, Limão, Manga tropical, Maçã, Pera, Tropical, Watermelon bomb, Frutas amarelas, Laranja, High passion, Duas goibas, Jabuticaba', price: '25,00' },
+            { name: 'Essências Sense', description: 'Melão com kiwi, Uva ice, Morango com melancia, Tutti frutti, Pera ice', price: '25,00' },
             { name: 'Trident', price: '4,00' },
             { name: 'Halls', price: '4,00' },
         ]
@@ -141,16 +130,18 @@ const menuData = {
             { name: 'Refrigerantes Lata', description: 'Coca-Cola, Coca Zero, Fanta, Guaraná', price: '6,00' },
             { name: 'Schweppes Lata', price: '7,00' },
             { name: 'Red Bull', description: 'Tradicional / Sabores', price: '13,00' },
-            { name: 'Monster Tradicional', description: 'Tradicional', price: '13,00' },
-            { name: 'Monster Mango Loco', description: 'Sabor Mango Loco', price: '13,00' },
+            { name: 'Red Bull Morango e Pêssego', price: '15,00' },
+            { name: 'Red Bull Blueberry', price: '15,00' },
+            { name: 'Monster Tradicional', price: '13,00' },
+            { name: 'Monster Mango Loco', price: '13,00' },
             { name: 'Suco Jarra', description: 'Limão, Maracujá, Morango', price: '15,00' },
         ]
     },
-    extras: { // NOVA CATEGORIA
+    extras: {
         title: 'Diversos / Extras',
         items: [
-            { name: 'Pow Pow (Confete de Festa)', description: '1 por R$30 ou 4 por R$100', price: '30,00' }, // NOVO ITEM
-            { name: 'Sinalizador', description: '', price: '20,00' }, // NOVO ITEM
+            { name: 'Pow Pow (Confete de Festa)', description: '1 por R$30 ou 4 por R$100', price: '30,00' },
+            { name: 'Sinalizador', description: '', price: '20,00' },
         ]
     }
 };
